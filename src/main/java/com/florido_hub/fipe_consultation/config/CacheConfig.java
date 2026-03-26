9949d1fcf177b9d5f0eb253e36d8ffc8981e4d6a
Florido-hub <floridodinizz@gmail.com>
2026-03-25 21:03:36 -0300

    chore: estrutura do projeto

---
+++ src/main/java/com/florido_hub/fipe_consultation/config/CacheConfig.java
@@ -0,0 +1,4 @@
+package com.florido_hub.fipe_consultation.config;
+
+public class CacheConfig {
+}
---
+++ src/main/java/com/florido_hub/fipe_consultation/dto/ConsultaFipeDTO.java
@@ -0,0 +1,4 @@
+package com.florido_hub.fipe_consultation.dto;
+
+public class ConsultaFipeDTO {
+}
