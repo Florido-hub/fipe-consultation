49d48d489a5fd2c9b27a7ff46d78a3e6dda932ea
Florido-hub <floridodinizz@gmail.com>
2026-03-25 21:42:32 -0300

    feat(repository): implementa Query no repository

--- src/main/java/com/florido_hub/fipe_consultation/dto/ConsultaFipeDTO.java
+++ src/main/java/com/florido_hub/fipe_consultation/dto/ConsultaFipeDTO.java
@@ -1,4 +1,13 @@
 package com.florido_hub.fipe_consultation.dto;
 
-public class ConsultaFipeDTO {
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public record ConsultaFipeDTO(
+        String marca,
+        String modelo,
+        Integer anoModelo,
+        BigDecimal preco,
+        String mesReferencia) implements Serializable {
+    private static final long serialVersionUID = 1L;
 }
--- src/main/java/com/florido_hub/fipe_consultation/repository/ReferenciaRepository.java
+++ src/main/java/com/florido_hub/fipe_consultation/repository/ReferenciaRepository.java
@@ -1,9 +1,27 @@
 package com.florido_hub.fipe_consultation.repository;
 
+import com.florido_hub.fipe_consultation.dto.ConsultaFipeDTO;
 import com.florido_hub.fipe_consultation.entity.ReferenciaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ReferenciaRepository extends JpaRepository<Long, ReferenciaEntity> {
+public interface ReferenciaRepository extends JpaRepository<ReferenciaEntity, Long> {
+
+    @Query("""
+            SELECT new com.florido_hub.fipe_consultation.dto.ConsultaFipeDTO(
+                ma.nome, mo.nome, r.anoModelo, r.preco, r.mesReferencia)
+            FROM ReferenciaEntity r
+                JOIN r.modelo mo
+                JOIN mo.marca ma
+            WHERE r.modelo.id = :modeloId AND r.anoModelo = :anoModelo
+            ORDER BY r.mesReferencia DESC
+            """)
+    List<ConsultaFipeDTO> findReferencias(
+            @Param("modeloId") Long modeloId,
+            @Param("anoModelo") Integer anoModelo);
 }
